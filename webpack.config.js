34cce6e40c8cd6d52e7ab3e71de656d080325129
Rahul Jayaraman <rahul.jayaraman@activesphere.com>
2016-02-26 10:27:13 +0530

    [webpack] update config
    be explicit about where bower_components lies
    also fix indentation

--- webpack.config.js
+++ webpack.config.js
@@ -3,61 +3,57 @@ var webpack = require("webpack");
 var BowerWebpackPlugin = require("bower-webpack-plugin");
 
 module.exports = {
-    entry:  {
-    	videoDetection: 'videoDetectionService',
-        popup: 'backbone/views/popup-view',
-        event: 'events'
-	},
-    output: {
-    	sourceMapFilename: 'bundle.js.map',
-        path:     'builds',
-        filename: '[name].bundle.js',
-    },
-    resolve: {
-        root: [
-        path.join(__dirname, "js")
-        ]
-    },
-    "devtool": '#inline-source-map',
-    module: {
-        preLoaders: [{
-            test: /\.js/,
-            exclude: /node_modules|bower_components|lib/,
-            loader: 'jscs-loader'
-        }, {
-            test: /\.js/,
-            exclude: /node_modules|bower_components|lib/,
-            loader: 'jshint-loader'
-        }],
-    	loaders: [
-            {
-                test:   /\.js/,
-                loader: 'babel',
-                include: __dirname + '/js',
-                exclude: /node_modules|bower_components/,
-                query: {
-	                presets: ['es2015']
-	              }
-            },
-
-        ],
-    },
-    plugins: [
-        new webpack.ResolverPlugin(
-            new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
-        ),
-        new BowerWebpackPlugin()
+  entry:  {
+    videoDetection: 'videoDetectionService',
+    popup: 'backbone/views/popup-view',
+    event: 'events'
+  },
+  output: {
+    sourceMapFilename: 'bundle.js.map',
+    path:     'builds',
+    filename: '[name].bundle.js',
+  },
+  resolve: {
+    root: [
+      path.join(__dirname, "js"),
+      path.join(__dirname, "bower_components")
+    ]
+  },
+  "devtool": '#inline-source-map',
+  module: {
+    preLoaders: [{
+      test: /\.js/,
+      exclude: /node_modules|bower_components|lib/,
+      loader: 'jscs-loader'
+    }, {
+      test: /\.js/,
+      exclude: /node_modules|bower_components|lib/,
+      loader: 'jshint-loader'
+    }],
+    loaders: [
+      {
+      test:   /\.js/,
+      loader: 'babel',
+      include: __dirname + '/js',
+      exclude: /node_modules|bower_components/,
+      query: {
+        presets: ['es2015']
+      }
+    }
     ],
-    jscs: {
-        preset: 'airbnb',
-        // By default the loader will try to pick up a `.jscsrc`
-        // file in the root of your project, but you can add any
-        // valid JSCS options here too.
-
-        emitErrors: true,
-        safeContextKeyword: ["_this", "self", "that"]
-      },
-      jshint: {
-        esnext: true
-          }
+  },
+  plugins: [
+    new webpack.ResolverPlugin(
+      new webpack.ResolverPlugin.DirectoryDescriptionFilePlugin(".bower.json", ["main"])
+    ),
+    new BowerWebpackPlugin()
+  ],
+  jscs: {
+    preset: 'airbnb',
+    emitErrors: true,
+    safeContextKeyword: ["_this", "self", "that"]
+  },
+  jshint: {
+    esnext: true
+  }
 };
