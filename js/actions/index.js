d58a05c8314ccfa32b99f835a23182480589937b
Rohitt Shinde <shinde.rohitt@gmail.com>
2016-07-11 18:24:46 +0530

    add actions, and reducers

---
+++ js/actions/index.js
@@ -0,0 +1,69 @@
+export const ADD_ANNOTATION = 'ADD_ANNOTATION';
+export const DELETE_ANNOTATION = 'DELETE_ANNOTATION';
+export const EDIT_ANNOTATION = 'EDIT_ANNOTATION';
+export const RECEIVE_INITIAL_STATE = 'RECEIVE_INITIAL_STATE';
+export const CHANGE_SEARCH_QUERY = 'CHANGE_SEARCH_QUERY';
+export const TOGGLE_HELP_MESSAGE = 'TOGGLE_HELP_MESSAGE';
+export const SET_METADATA = 'SET_METADATA';
+
+
+export const receiveInitialState = (state) => {
+  return {
+    type: RECEIVE_INITIAL_STATE,
+    state: {
+      ...state,
+      notes: state.annotations,
+    },
+  }
+}
+
+export const addAnnotation =
+(text, metadata, start_seconds, currentVideoTime) => {
+  return {
+    type: ADD_ANNOTATION,
+    timeNow: new Date().toString(),
+    id: Date.now(),
+    text,
+    metadata,
+    start_seconds,
+    currentVideoTime,
+  }
+}
+
+export const deleteAnnotation = (id) => {
+  return {
+    type: DELETE_ANNOTATION,
+    id,
+  }
+}
+
+export const editAnnotation =
+(id, text, metadata) => {
+  return {
+    type: EDIT_ANNOTATION,
+    timeNow: new Date().toString(),
+    id,
+    text,
+    metadata,
+  }
+}
+
+export const setMetadata = (pagedata) => {
+  return {
+    type: SET_METADATA,
+    pagedata,
+  };
+}
+
+export const changeSearchQuery = (text) => {
+  return {
+    type: CHANGE_SEARCH_QUERY,
+    query: text,
+  }
+}
+
+export const toggleHelpMessage = () => {
+  return {
+    type: TOGGLE_HELP_MESSAGE,
+  }
+}
---
+++ js/reducers/index.js
@@ -0,0 +1,124 @@
+import {
+  RECEIVE_INITIAL_STATE,
+  ADD_ANNOTATION, EDIT_ANNOTATION, DELETE_ANNOTATION,
+  CHANGE_SEARCH_QUERY, TOGGLE_HELP_MESSAGE, SET_METADATA,
+} from '../actions/index';
+
+import Utils from '../utils';
+
+import { combineReducers } from 'redux';
+
+
+const initialMetadata = {
+    videoTitle: '',
+    provider: '',
+    creationTime: null,
+    lastUpdate: null,
+};
+
+const notes = (state = [], action) => {
+  switch (action.type) {
+    case RECEIVE_INITIAL_STATE:
+      return [ ...action.state.notes ];
+      
+    case ADD_ANNOTATION:
+      const id = action.id;
+      const start_seconds = action.start_seconds;
+      const end_seconds = action.currentVideoTime;
+      const start_minutes = Utils.minuteSeconds(start_seconds);
+      const end_minutes = Utils.minuteSeconds(end_seconds);
+
+      const annotationObj = {
+        id,
+        start_seconds,
+        end_seconds,
+        start_minutes,
+        end_minutes,
+        ...Utils.splitAnnotation(action.text),
+      }
+      
+      const currentNotes = [ ...state ];
+      const newNotes = currentNotes
+        .filter((each) =>
+          each.start_seconds <= start_seconds
+        )
+        .concat([ annotationObj ])
+        .concat(currentNotes.filter((each) =>
+          each.start_seconds > start_seconds
+        ));
+      
+      return newNotes;
+
+    case DELETE_ANNOTATION:
+      return state.filter((each) => each.id !== action.id);
+
+    case EDIT_ANNOTATION:
+      return state
+        .map((each) => {
+          if(each.id !== action.id) return each;
+          return {
+            ...each,
+            ...Utils.splitAnnotation(action.text)
+          }
+        });
+
+    default:
+      return state;
+  }
+}
+
+const metadata = (state = initialMetadata, action) => {
+  switch (action.type) {
+    case RECEIVE_INITIAL_STATE:
+      return { ...action.state.metadata };
+
+    case SET_METADATA:
+      return {
+        ...state,
+        ...action.pagedata,
+      };
+      
+    case ADD_ANNOTATION:
+      return {
+        ...action.metadata,
+        creationTime: action.timeNow,
+        lastUpdate: action.timeNow,
+      };
+
+    case EDIT_ANNOTATION:
+      return {
+        ...action.metadata,
+        lastUpdate: action.timeNow,
+      };
+
+    default:
+      return state;
+  }
+}
+
+const searchQuery = (state = '', action) => {
+  switch (action.type) {
+    case CHANGE_SEARCH_QUERY:
+      return action.query;
+    default:
+      return state;
+  }
+}
+
+const helpMessageShown = (state = false, action) => {
+  switch (action.type) {
+    case TOGGLE_HELP_MESSAGE:
+      return !state;
+    default:
+      return state;
+  }
+}
+
+const rootReducer = combineReducers({
+  notes,
+  metadata,
+  searchQuery,
+  helpMessageShown,
+});
+
+export default rootReducer;
