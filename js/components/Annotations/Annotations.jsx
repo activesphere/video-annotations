fb7a14b44433e816a84449be32330c73e12098bb
Rohitt Shinde <shinde.rohitt@gmail.com>
2016-07-12 13:24:44 +0530

    move annotation creation code to (relevant) VideoAnnotation component

--- js/components/Annotations/Annotations.jsx
+++ js/components/Annotations/Annotations.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
 import AnnotationItem from '../AnnotationItem/AnnotationItem';
-import Editor from '../Editor/Editor';
 import Utils from '../../utils';
 
 import './Annotations.less';
 
-import { addAnnotation, deleteAnnotation,
-         editAnnotation, setMetadata } from '../../actions';
+import { deleteAnnotation, setMetadata } from '../../actions';
 
 class Annotations extends React.Component {
   constructor(props) {
@@ -19,55 +16,18 @@ class Annotations extends React.Component {
 
     this.videoTag = Utils.getVideoInterface();
 
-    this.onKeyDown = this.onKeyDown.bind(this);
-    this.onAnnotationCreate = this.onAnnotationCreate.bind(this);
-    this.removeEditor = this.removeEditor.bind(this);
     this.onItemDelete = this.onItemDelete.bind(this);
     this.onItemEdit = this.onItemEdit.bind(this);
     this.onSeek = this.onSeek.bind(this);
-    this.insertEditor = this.insertEditor.bind(this);
     this.updateMetadata = this.updateMetadata.bind(this);
   }
 
   componentDidMount() {
     this.store = this.context.store;
     this.unsubscribe = this.store.subscribe(() => this.forceUpdate());
-
-    document.onkeydown = this.onKeyDown;
   }
 
   componentWillUnmount() { this.unsubscribe(); }
-
-  onKeyDown(e) {
-    if (e.altKey && e.key === 'd') {
-      // bring up the editor to create a new annotation.
-      e.preventDefault();
-      this.insertEditor();
-    }
-  }
-
-  onAnnotationCreate(start_seconds, text, editId) {
-    const state = this.store.getState();
-    if (editId) {
-      // this is to edit, not to create
-      this.store.dispatch(editAnnotation(
-        editId,
-        text,
-        state.metadata
-      ));
-    } else {
-      // creating a new note
-      this.store.dispatch(addAnnotation(
-        text,
-        state.metadata,
-        start_seconds,
-        this.videoTag.getCurrentTime()
-      ));
-    }
-    
-    // remove editor
-    this.removeEditor();
-  }
 
   onItemDelete(id) {
     return () => {
@@ -80,36 +40,12 @@ class Annotations extends React.Component {
       const state = this.store.getState();
       const targetAnnotation = state
         .notes.find((note) => note.id === id);
-      this.insertEditor(targetAnnotation);
+      this.props.insertEditor(targetAnnotation);
     };
   }
 
   onSeek(toTime) {
     this.videoTag.setCurrentTime(toTime);
-  }
-
-  insertEditor(annotation = null) {
-    const start_seconds = annotation ?
-                          annotation.start_seconds :
-                          Number(this.videoTag.getCurrentTime());
-    this.videoTag.pause();
-    ReactDOM.render(
-      <Editor
-        handleAnnotationCreate={this.onAnnotationCreate}
-        handleAnnotationCancel={this.removeEditor}
-        annotation={annotation}
-        start_seconds={start_seconds}
-        videoTag={this.videoTag}
-      />,
-      document.querySelector('.create-annotation')
-    );
-  }
-
-  removeEditor() {
-    ReactDOM.unmountComponentAtNode(
-      document.querySelector('.create-annotation')
-    );
-    this.videoTag.play();
   }
 
   updateMetadata() {
@@ -150,6 +86,10 @@ class Annotations extends React.Component {
 
 Annotations.contextTypes = {
   store: React.PropTypes.object,
+};
+
+Annotations.propTypes = {
+  insertEditor: React.PropTypes.func,
 };
 
 export default Annotations;
--- js/containers/VideoAnnotation/VideoAnnotation.jsx
+++ js/containers/VideoAnnotation/VideoAnnotation.jsx
@@ -1,29 +1,99 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import SearchBox from '../../components/SearchBox/SearchBox';
+import Editor from '../../components/Editor/Editor';
 
 import HelpMessage from '../../components/HelpMessage/HelpMessage';
 import UserInfo from '../../components/UserInfo/UserInfo';
 import Annotations from '../../components/Annotations/Annotations';
+import Utils from '../../utils';
 
 import './VideoAnnotation.less';
 
-import { changeSearchQuery, toggleHelpMessage } from '../../actions';
+import { changeSearchQuery, toggleHelpMessage,
+         editAnnotation, addAnnotation } from '../../actions';
 
 
 class VideoAnnotation extends React.Component {
+  constructor() {
+    super();
+    this.videoTag = Utils.getVideoInterface();
+    
+    this.onKeyDown = this.onKeyDown.bind(this);
+    this.onAnnotationCreate = this.onAnnotationCreate.bind(this);
+    this.insertEditor = this.insertEditor.bind(this);
+    this.removeEditor = this.removeEditor.bind(this);
+  }
+  
   componentDidMount() {
     this.unsubscribe = this.store.subscribe(() => this.forceUpdate());
+    document.onkeydown = this.onKeyDown;
   }
 
   componentWillUnmount() { this.unsubscribe(); }
 
+  onKeyDown(e) {
+    if (e.altKey && e.key === 'd') {
+      // bring up the editor to create a new annotation.
+      e.preventDefault();
+      this.insertEditor();
+    }
+  }
+
+  onAnnotationCreate(start_seconds, text, editId) {
+    const state = this.store.getState();
+    if (editId) {
+      // this is to edit, not to create
+      this.store.dispatch(editAnnotation(
+        editId,
+        text,
+        state.metadata
+      ));
+    } else {
+      // creating a new note
+      this.store.dispatch(addAnnotation(
+        text,
+        state.metadata,
+        start_seconds,
+        this.videoTag.getCurrentTime()
+      ));
+    }
+    
+    // remove editor
+    this.removeEditor();
+  }
+
+  insertEditor(annotation = null) {
+    const start_seconds = annotation ?
+                          annotation.start_seconds :
+                          Number(this.videoTag.getCurrentTime());
+    this.videoTag.pause();
+    ReactDOM.render(
+      <Editor
+        handleAnnotationCreate={this.onAnnotationCreate}
+        handleAnnotationCancel={this.removeEditor}
+        annotation={annotation}
+        start_seconds={start_seconds}
+        videoTag={this.videoTag}
+      />,
+      document.querySelector('.create-annotation')
+    );
+  }
+
+  removeEditor() {
+    ReactDOM.unmountComponentAtNode(
+      document.querySelector('.create-annotation')
+    );
+    this.videoTag.play();
+  }
+  
   render() {
     this.store = this.context.store;
     const state = this.store.getState();
     return (
       <div id="video-annotation">
-        <div className="sidebar sidebar-hidden">
+        <div className="sidebar sidebar-visible">
           <div className="annotations-list">
             <UserInfo />
             <SearchBox
@@ -50,7 +120,7 @@ class VideoAnnotation extends React.Component {
             </div>
             <div className="create-annotation"></div>
             <HelpMessage visibility={state.helpMessageShown} />
-            <Annotations />
+            <Annotations insertEditor={this.insertEditor} />
           </div>
         </div>
       </div>
