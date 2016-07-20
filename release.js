c1db393dba1675be97fee5e1cbf6c190634456d8
Rohitt Shinde <shinde.rohitt@gmail.com>
2016-07-20 17:19:04 +0530

    fix source extension path, and syntaxes in release script

--- release.js
+++ release.js
@@ -13,73 +13,73 @@ buildZipFile();
 gitCommitAndTag();
 
 function getJSON(fileName) {
-	const data = fs.readFileSync(path.join(__dirname, fileName), 'utf8');
-	return JSON.parse(data.slice().toString());
+  const data = fs.readFileSync(path.join(__dirname, fileName), 'utf8');
+  return JSON.parse(data.slice().toString());
 }
 
 function bumpRelease() {
-	const version = packageJSON.version.split('.');
-	const releaseType = process.argv[2];
+  const version = packageJSON.version.split('.');
+  const releaseType = process.argv[2];
 
-	if (!releaseType) {
-		++version[2];
+  if (!releaseType) {
+    ++version[2];
 
-	} else if (releaseType === 'minor') {
-		++version[1];
-		version[2] = 0;
-	} else {
-		++version[0];
-		version[1] = version[2] = 0;
-	}
+  } else if (releaseType === 'minor') {
+    ++version[1];
+    version[2] = 0;
+  } else {
+    ++version[0];
+    version[1] = version[2] = 0;
+  }
 
-	global.versionStr = version.join('.');
+  global.versionStr = version.join('.');
 
-	packageJSON.version = manifestJSON.version = versionStr;
-	fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(packageJSON, null, '  '));
-	fs.writeFileSync(path.join(__dirname, 'manifest.json'), JSON.stringify(manifestJSON, null, '  '));
+  packageJSON.version = manifestJSON.version = versionStr;
+  fs.writeFileSync(path.join(__dirname, 'package.json'), JSON.stringify(packageJSON, null, '  '));
+  fs.writeFileSync(path.join(__dirname, 'manifest.json'), JSON.stringify(manifestJSON, null, '  '));
 }
 
 function buildZipFile() {
 
-	npmBuild('production');
-	createDistDir();
-	createZipFile();
+  npmBuild('production');
+  createDistDir();
+  createZipFile();
 
-	function npmBuild(env) {
-		process.env.NODE_ENV = env;
-		execSync('npm run build');
-	}
+  function npmBuild(env) {
+    process.env.NODE_ENV = env;
+    execSync('npm run build');
+  }
 
-	function createDistDir() {
-		var dir = path.join(__dirname, 'dist');
+  function createDistDir() {
+    var dir = path.join(__dirname, 'dist');
 
-		try {
-			fs.accessSync(dir);
-		} catch (e) {
-		    fs.mkdirSync(dir);
-		}
-	}
+    try {
+      fs.accessSync(dir);
+    } catch (e) {
+      fs.mkdirSync(dir);
+    }
+  }
 
-	function createZipFile() {
-		const output = fs.createWriteStream('dist/video-annotation-' + versionStr + '.zip');
-		const archive = archiver('zip');
+  function createZipFile() {
+    const output = fs.createWriteStream('dist/video-annotation-' + versionStr + '.zip');
+    const archive = archiver('zip');
 
-		archive.pipe(output);
-		archive.bulk([
-		    { expand: true, cwd: 'builds', src: ['**']}
-		]);
-		archive.finalize();
+    archive.pipe(output);
+    archive.bulk([
+      { expand: true, cwd: 'builds/extension', src: ['**']}
+    ]);
+    archive.finalize();
 
-		output.on('close', () => {
-			// rebuild /builds extension with sourcemap enabled
-			npmBuild('');
-		});
-	}
+    output.on('close', () => {
+      // rebuild /builds extension with sourcemap enabled
+      npmBuild('');
+    });
+  }
 
 }
 
 function gitCommitAndTag() {
-	execSync('git add manifest.json package.json');
-	execSync('git commit -m ' + versionStr);
-	execSync('git tag ' + versionStr);
+  execSync('git add manifest.json package.json');
+  execSync('git commit -m ' + versionStr);
+  execSync('git tag ' + versionStr);
 }
