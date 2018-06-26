bce8571cec463c898693b906065c4711b27ea4d4
ciju <ciju.ch3rian@gmail.com>
2018-06-27 00:13:18 +0530

    Lint

--- src/js/controllers/video-scribe-controller.js
+++ src/js/controllers/video-scribe-controller.js
@@ -1,16 +1,14 @@
-import { Controller } from "stimulus";
-import youtubeRegex from "youtube-regex";
-import formatTime from '../lib/format-time';
+import {Controller} from 'stimulus';
+import youtubeRegex from 'youtube-regex';
 import hotkeys from 'hotkeys-js';
+import formatTime from '../lib/format-time.js';
 
-hotkeys.filter = function(event) {
-	const tagName = (event.target || event.srcElement).tagName;
-	return tagName == 'TEXTAREA';
-}
+hotkeys.filter = function (event) {
+	const {tagName} = (event.target || event.srcElement);
+	return tagName === 'TEXTAREA';
+};
 
 export default class extends Controller {
-	static targets = [ "editor" ];
-
 	initialize() {
 		this.player = undefined;
 		this.playerState = -1;
@@ -33,26 +31,27 @@ export default class extends Controller {
 
 	initializePlayer(id) {
 		const tag = document.createElement('script');
-		tag.src = "//www.youtube.com/iframe_api";
+		tag.src = '//www.youtube.com/iframe_api';
 		const firstScriptTag = document.getElementsByTagName('script')[0];
 		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-		let _this = this;
+		const _this = this;
 
 		function onYouTubeIframeAPIReady() {
+			const {YT} = window;
 			_this.player = new YT.Player('video-placeholder', {
 				height: '360',
 				width: '640',
 				videoId: id,
 				playerVars: {
-					'autoplay': 1,
-					'modestbranding': 1,
-					'rel': 0,
-					'showinfo': 0
+					autoplay: 1,
+					modestbranding: 1,
+					rel: 0,
+					showinfo: 0
 					// 'controls': 0
 				},
 				events: {
-					'onReady': onPlayerReady,
-					'onStateChange': _this.onPlayerStateChange
+					onReady: onPlayerReady,
+					onStateChange: _this.onPlayerStateChange
 				}
 			});
 		}
@@ -66,8 +65,8 @@ export default class extends Controller {
 
 	onPlayerStateChange(event) {
 		if (this.playerState === -1) {
-			const title = this.player.getVideoData().title;
-			if (this.editorTarget.value.length == 0) {
+			const {title} = this.player.getVideoData();
+			if (this.editorTarget.value.length === 0) {
 				this.editorTarget.value = `# ${title}\n`;
 				this.editorTarget.focus();
 			}
@@ -76,21 +75,22 @@ export default class extends Controller {
 	}
 
 	connect() {
-		const videoUrl = new URL(location.href).searchParams.get('url');
-		if (videoUrl && videoUrl !== "") {
+		const videoUrl = new URL(document.location.href).searchParams.get('url');
+		if (videoUrl && videoUrl !== '') {
 			const id = youtubeRegex().exec(videoUrl).reverse()[0];
 			this.initializePlayer(id);
 			this.initializeKeyboardShortcuts();
 		}
 	}
 
-	appendTimeStamp(e, handler) {
+	appendTimeStamp() {
 		const timestamp = formatTime(this.player.getCurrentTime());
-		const newline = this.editorTarget.value.length ? "\n\n" : "";
+		const newline = this.editorTarget.value.length ? '\n\n' : '';
 		this.editorTarget.value += `${newline}## /{${timestamp}}/: `;
 	}
 
-	togglePlay(e, handler) {
+	togglePlay() {
+		const {YT} = window;
 		if (this.playerState === YT.PlayerState.PLAYING) {
 			this.player.pauseVideo();
 		} else {
@@ -103,14 +103,18 @@ export default class extends Controller {
 	}
 
 	jump10sBackward() {
+		const {YT} = window;
 		if (this.playerState === YT.PlayerState.PLAYING) {
 			this.seekTo(this.player.getCurrentTime() - 10);
 		}
 	}
 
 	jump10sForward() {
+		const {YT} = window;
 		if (this.playerState === YT.PlayerState.PLAYING) {
 			this.seekTo(this.player.getCurrentTime() + 10);
 		}
 	}
 }
+
+Controller.targets = ['editor'];
