fa96e175ee4b9437b5666605457ecd004a81803f
Soumik Rakshit <soumikrakshit@yandex.com>
2019-01-17 19:34:23 +0530

    Towards making the extension. Ejected from create-react-app.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { App, EditorComponent } from './App';
 import * as serviceWorker from './serviceWorker';
-import TestRenderer from 'react-test-renderer';
 
 ReactDOM.render(<App />, document.getElementById('body'));
-
-// TestRenderer.create(<EditorComponent />);
 
 // If you want your app to work offline and load faster, you can change unregister() to register()
 // below. Note this comes with some pitfalls. Learn more about service workers: http://bit.ly/CRA-
