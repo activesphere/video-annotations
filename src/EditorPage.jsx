876aa4881c1358517f41ecfaa2c52c4cc396048c
Soumik Rakshit <soumikrakshit@yandex.com>
2019-03-06 20:07:52 +0530

    Make autosave while typing more responsive.

--- src/EditorPage.jsx
+++ src/EditorPage.jsx
@@ -119,7 +119,6 @@ class EditorPage extends Component {
         startingVideoTime: PropTypes.number,
         startingPopperMessage: PropTypes.string,
         showInfo: PropTypes.func.isRequired,
-        getNoteMenuItems: PropTypes.func.isRequired,
     };
 
     static defaultProps = {
