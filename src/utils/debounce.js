c4d0a8248f377bd8fb200c4033dbc4b025fd7e4a
ciju <ciju.ch3rian@gmail.com>
2019-03-10 22:52:20 +0530

    chore: add debounce.js

---
+++ src/utils/debounce.js
@@ -0,0 +1,13 @@
+/* https://medium.com/@TCAS3/debounce-deep-dive-javascript-es6-e6f8d983b7a1 */
+const debounce = (fn, time) => {
+    let timeout;
+
+    return function() {
+        const functionCall = () => fn.apply(this, arguments);
+
+        clearTimeout(timeout);
+        timeout = setTimeout(functionCall, time);
+    };
+};
+
+export default debounce;
