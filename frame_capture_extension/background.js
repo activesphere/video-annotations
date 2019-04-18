a26255145bfe8393540ab2628a2f1523e66bedd6
Soumik Rakshit <soumik@activesphere.com>
2019-04-18 15:20:28 +0530

    Adding frame capture extension

---
+++ frame_capture_extension/background.js
@@ -0,0 +1,27 @@
+// listen for our browerAction to be clicked
+browser.browserAction.onClicked.addListener(tab => {
+    // for the current tab, inject the "inject.js" file & execute it
+    console.log('Inserting script in tab', tab);
+    browser.tabs.executeScript(tab.id, {
+        file: 'inject.js',
+        allFrames: true,
+    });
+});
+
+// TODO: Not using this. Remove it.
+browser.runtime.onConnect.addListener(port => {
+    if (port.name !== 'vid_annot_port') {
+        console.log('Got connection from unexpected port -', port);
+        return;
+    }
+    console.log('Connection at port', port);
+    port.onMessage.addListener(msg => {
+        msg = JSON.parse(msg);
+
+        if (msg.type === 'logging') {
+            console.log('Log -', msg.text);
+            return;
+        }
+        console.log('Unknown message type -', msg);
+    });
+});
---
+++ frame_capture_extension/inject.js
@@ -0,0 +1,79 @@
+(function() {
+    const YOUTUBE_IFRAME_ID = '__yt_iframe__';
+    const VID_ANNOT_ROOT_ID = '__vid_annot_root__';
+    const CANVAS_ID = '__image_destination_canvas__';
+
+    console.log('Called inject.js');
+
+    // Port to background script
+    let port = null;
+
+    // Canvas. Created only in the youtube iframe to store the current video frame and get a dataURI.
+    let canvasRef = null;
+
+    // Returns reference to the destination canvas used to store the image from the video. Creates
+    // it if it isn't available.
+    function getCanvasElement(width = 400, height = 300) {
+        let canvas = document.getElementById(CANVAS_ID);
+        if (!canvas) {
+            canvas = document.createElement('canvas');
+            canvas.id = CANVAS_ID;
+
+            // Just for now. Will not be showing the canvas to user.
+            canvas.style.position = 'fixed';
+            canvas.style.top = 0;
+            canvas.style.right = 0;
+            // document.body.appendChild(canvas);
+        }
+        canvas.width = width;
+        canvas.height = height;
+        canvasRef = canvas;
+        return canvas;
+    }
+
+    function getCurrentFrameURI(canvasRef, videoElement) {
+        // TODO: perhaps create a single context and don't re-create over and over?
+        const C = canvasRef.getContext('2d');
+        C.drawImage(videoElement, 0, 0, canvasRef.width, canvasRef.height);
+        const url = canvasRef.toDataURL('image/png');
+        return url;
+    }
+
+    function main() {
+        // Get the <video> element.
+        const videoElement = document.querySelector(`video`);
+
+        const scriptIsInIframe = !!videoElement;
+
+        if (!scriptIsInIframe) {
+            return;
+        }
+
+        console.log('Yeah bruh, content script is in frame');
+
+        // Listen for message from main app...
+        window.addEventListener(
+            'message',
+            e => {
+                if (e.data.type === 'VID_ANNOT_CAPTURE_CURRENT_FRAME') {
+                    console.log('Content script received message requesting current frame');
+
+                    // ... then get the frame and send back to app.
+                    try {
+                        const dataUrl = getCurrentFrameURI(getCanvasElement(), videoElement);
+                        console.log('dataUrl =', dataUrl);
+                        window.parent.postMessage(
+                            { type: 'VID_ANNOT_CAPTURED_FRAME', dataUrl },
+                            '*'
+                        );
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+            },
+            false
+        );
+    }
+
+    main();
+})();
