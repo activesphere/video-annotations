0c553800167e2b7f39501ba15526b45d43c99831
Soumik Rakshit <soumik@activesphere.com>
2019-04-23 15:37:51 +0530

    Show DropboxLogin screen path /dropbox_oauth

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ import { Paper, Tabs, Tab } from '@material-ui/core';
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter, Switch, Route, Link, Redirect } from 'react-router-dom';
 import { SnackbarContextProvider } from './context/SnackbarContext';
+import DropboxLogin from './DropboxLogin';
+import { Dropbox } from 'dropbox';
+import dropboxHelper, { initDropboxHelper } from './DropboxHelper';
+import * as LS from './LocalStorageHelper';
 
 import theme from './mui_theme';
 
@@ -18,63 +22,126 @@ const getTabValue = path => {
     return null;
 };
 
+// prettier-ignore
+console.log(`Dropbox Key = ${process.env.REACT_APP_DROPBOX_KEY}, Secret=${process.env.REACT_APP_DROPBOX_SECRET}`);
+
 const Main = ({ ytAPI }) => {
     const [lastVideoId, setLastVideoId] = useState(undefined);
 
+    const [dropboxSetupComplete, setDropboxSetupComplete] = useState(false);
+    // ^ Means that we have initialized the dropbox api and also made sure the notes folder
+    // is available there and synced localstorage with dropbox.
+
+    const [dropboxSetupFailed, setDropboxSetupFailed] = useState(false);
+
+    const handleTokenSubmit = (accessToken, idToNoteData) => {
+        const dbx = new Dropbox({ accessToken, clientId: process.env.REACT_APP_DROPBOX_KEY });
+        const p = initDropboxHelper(dbx);
+
+        p.then(() => {
+            const p1 = LS.syncWithDropbox(LS.idToNoteData);
+            p1.then(() => {
+                console.log('setup dropbox complete');
+                setDropboxSetupComplete(true);
+                console.log('Dropbox setup complete');
+            }).catch(error => {
+                console.log('Failed to sync with dropbox - ', error);
+            });
+        }).catch(err => {
+            console.log(err);
+            setDropboxSetupFailed(true);
+            console.log('Failed to set up Dropbox');
+        });
+    };
+
     return (
         <BrowserRouter>
-            <Route
-                path="/"
-                render={({ location }) => (
-                    <>
-                        <Paper elevation={0}>
-                            <Tabs
-                                indicatorColor="primary"
-                                textColor="primary"
-                                value={getTabValue(location.pathname)}
-                                centered
-                            >
-                                <Tab value="editor" label="Editor" component={Link} to="/editor" />
-                                <Tab
-                                    value="notes"
-                                    label="Saved notes"
-                                    component={Link}
-                                    to="/saved_notes"
+            <Switch>
+                <Route
+                    path="/dropbox_oauth"
+                    render={() => {
+                        if (dropboxSetupComplete) {
+                            return <Redirect to="/editor" />;
+                        } else if (dropboxHelper.isInitialized() && !dropboxSetupFailed) {
+                            // Means we have created the api but have not finished setting up the notes folder on dropbox side.
+                            // Disabling handleTokenSubmit.
+                            return <DropboxLogin idToNoteData={LS.idToNoteData} />;
+                        }
+                        // User didn't click submit token button
+                        return (
+                            <>
+                                <DropboxLogin
+                                    handleTokenSubmit={handleTokenSubmit}
+                                    idToNoteData={LS.idToNoteData}
                                 />
-                            </Tabs>
-                        </Paper>
-                        <Switch>
-                            <Route path="/saved_notes" component={NotesPage} />
-                            <Route
-                                path="/editor/:videoId"
-                                render={({ match }) => {
-                                    const { videoId } = match.params;
-                                    if (videoId) {
-                                        setLastVideoId(videoId);
-                                    }
+                            </>
+                        );
+                    }}
+                />
+                <Route
+                    path="/"
+                    render={({ location }) => (
+                        <>
+                            <Paper elevation={0}>
+                                <Tabs
+                                    indicatorColor="primary"
+                                    textColor="primary"
+                                    value={getTabValue(location.pathname)}
+                                    centered
+                                >
+                                    <Tab
+                                        value="editor"
+                                        label="Editor"
+                                        component={Link}
+                                        to="/editor"
+                                    />
+                                    <Tab
+                                        value="notes"
+                                        label="Saved notes"
+                                        component={Link}
+                                        to="/saved_notes"
+                                    />
+                                </Tabs>
+                            </Paper>
+                            <Switch>
+                                <Route path="/saved_notes" render={() => <NotesPage />} />
+                                <Route
+                                    path="/editor/:videoId"
+                                    render={({ match }) => {
+                                        const { videoId } = match.params;
+                                        if (videoId) {
+                                            setLastVideoId(videoId);
+                                        }
 
-                                    return (
-                                        <EditorPage
-                                            key={videoId}
-                                            ytAPI={ytAPI}
-                                            startingVideoId={videoId}
-                                        />
-                                    );
-                                }}
-                            />
-                            <Route
-                                path="/editor"
-                                render={() => {
-                                    if (lastVideoId)
-                                        return <Redirect to={`/editor/${lastVideoId}`} />;
+                                        return (
+                                            <EditorPage
+                                                key={videoId}
+                                                ytAPI={ytAPI}
+                                                startingVideoId={videoId}
+                                            />
+                                        );
+                                    }}
+                                />
+                                <Route
+                                    path="/editor"
+                                    render={() => {
+                                        if (lastVideoId)
+                                            return <Redirect to={`/editor/${lastVideoId}`} />;
 
-                                    return <EditorPage ytAPI={ytAPI} />;
-                                }}
-                            />
-                        </Switch>
-                    </>
-                )}
-            />
+                                        return <EditorPage ytAPI={ytAPI} />;
+                                    }}
+                                />
+
+                                {/* Redirect to dropbox oauth token input page otherwise */}
+                                <Route
+                                    path="/"
+                                    render={props => <Redirect to={'/dropbox_oauth/'} />}
+                                />
+                            </Switch>
+                        </>
+                    )}
+                />
+            </Switch>
         </BrowserRouter>
     );
 };
