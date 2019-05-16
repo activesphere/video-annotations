cdd90a185c65e35a6dd7d7065e23fd5c9af47747
Soumik Rakshit <soumik@activesphere.com>
2019-05-16 18:24:51 +0530

    Keep dist/ directory empty. Copy required files on build.

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './inject.js',
@@ -6,4 +7,12 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: 'inject.bundled.js',
     },
+
+    plugins: [
+        new CopyPlugin([
+            { from: './background.js', to: 'background.js' },
+            { from: './manifest.json', to: 'manifest.json' },
+            { from: './va.png', to: 'va.png' },
+        ]),
+    ],
 };
