5a9799cee9775888dac4614125c2bcb953ba313c
Soumik Rakshit <soumik@activesphere.com>
2019-05-17 16:54:13 +0530

    Make extension automatically detect vid-annot app.
    No user intervention required. Automatically loads inject script.

--- extension/background.js
+++ extension/background.js
@@ -2,28 +2,18 @@ import ns from './browser_namespace';
 
 const State = {
     // On click, should first load the controller script
-    shouldLoadControllerScript: true,
+    shouldLoadControllerScript: false,
 
-    // Port for communicating with currently active controller script
-    port: null,
+    controlScriptPort: null,
 
     // Which tab the content script was injected. If closed, we reset the background scripts state
     tabId: null,
     windowId: null,
 };
 
-function portListener(p) {
-    console.log('Received connection from port -', p);
-
-    if (p.name !== 'VID_ANNOT_CONTROL_SCRIPT_PORT') {
-        return;
-    }
-
-    State.port = p;
-
-    State.port.onDisconnect;
-
-    State.port.onMessage.addListener(m => {
+function connectedToControlScriptPort(p) {
+    State.controlScriptPort = p;
+    State.controlScriptPort.onMessage.addListener(m => {
         console.log('Received message', m);
 
         switch (m.command) {
@@ -43,43 +33,35 @@ function portListener(p) {
     });
 }
 
-// listen for our browerAction to be clicked
-ns.browserAction.onClicked.addListener(tab => {
-    if (State.shouldLoadControllerScript) {
-        State.tabId = tab.id;
-        State.windowId = tab.windowId;
+ns.runtime.onConnect.addListener(connectedToControlScriptPort);
 
-        console.log('Loading control script...');
+function onTabUpdated(tabId, changeInfo, tabInfo) {
+    console.log(
+        'va-extension: background.js - tabInfo =',
+        tabInfo,
+        'changeInfo.status and tabInfo.url =',
+        changeInfo.status,
+        tabInfo.url
+    );
 
-        State.shouldLoadControllerScript = false;
-
-        ns.runtime.onConnect.addListener(portListener);
-
-        ns.tabs.executeScript(tab.id, {
+    if (changeInfo.status === 'complete') {
+        console.log('Loading content script in tab with url -', tabInfo.url);
+        // Load the control script
+        ns.tabs.executeScript(tabId, {
             file: 'control.bundled.js',
-            allFrames: true,
         });
-
-        ns.tabs.onRemoved.addListener((tabId, { windowId }) => {
-            console.log('tabId = ', tabId, 'windowId =', windowId);
-            if (tabId === State.tabId && windowId === State.windowId) {
-                console.log('Resetting');
-                State.shouldLoadControllerScript = true;
-
-                if (State.port) {
-                    State.port.onMessage.removeListener(portListener);
-                    State.port.disconnect();
-                    State.port = null;
-                }
-
-                State.tabId = null;
-                State.windowId = null;
-            }
-        });
-
-        console.log('...Loaded control script');
-    } else {
-        // TODO: remove this
-        console.log('Not loading control script');
     }
-});
+}
+
+/*
+function onNewTabCreated(tab) {
+    console.log('va-extension: background.js - Loading control.js into tab', tab.id);
+
+    ns.tabs.executeScript(tab.id, {
+        file: 'control.bundled.js',
+    });
+}
+*/
+
+console.log('va-extension: background.js - Installed');
+ns.tabs.onUpdated.addListener(onTabUpdated);
--- extension/control.js
+++ extension/control.js
@@ -7,13 +7,47 @@ const State = {
     port: null,
 };
 
-State.port = ns.runtime.connect({ name: 'VID_ANNOT_CONTROL_SCRIPT_PORT' });
+// Control script is loaded after tab's status is 'complete'. So this it's ok to just do the thing
+// we want to do immediately.
+main();
 
-console.log('State.port =', State.port);
+// Inject an empty div into the document to mark this page as inspected. Returns true if page was
+// already marked, otherwise returns false.
+function markPageAsInspected() {
+    const contentScriptLoaded = document.getElementById('__content_script_div__');
 
-window.addEventListener('message', m => {
-    if (m.data.type === AppConfig.LoadInjectScriptMessage) {
-        console.log('control.js: Telling bg script to load inject script');
-        State.port.postMessage({ command: 'SHOULD_LOAD_INJECT_SCRIPT' });
+    if (!contentScriptLoaded) {
+        const markerElement = document.createElement('div');
+        markerElement.id = '__content_script_div__';
+        document.body.appendChild(markerElement);
+        return false;
     }
-});
+
+    return true;
+}
+
+function main() {
+    console.log('control.js: main()');
+    const root = document.getElementById(AppConfig.VidAnnotRootId);
+    if (!root) {
+        console.log('va-extension: control.js - Page is not vid-annot app. So not doing anything.');
+        return;
+    }
+
+    if (markPageAsInspected()) {
+        console.log('va-extension: control.js - Page is already marked. Not doing anything.');
+        return;
+    }
+
+    console.log('va-extension: control.js - Marked page. Loading worker.js');
+
+    window.addEventListener('message', m => {
+        if (m.data.type === AppConfig.LoadInjectScriptMessage) {
+            console.log('control.js: Telling bg script to load inject script');
+            State.port.postMessage({ command: 'SHOULD_LOAD_INJECT_SCRIPT' });
+        }
+    });
+
+    State.port = ns.runtime.connect({ name: 'VID_ANNOT_CONTROL_SCRIPT_PORT' });
+    State.port.postMessage({ type: 'IS_VID_ANNOT_APP' });
+}
