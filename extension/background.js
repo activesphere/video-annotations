a54a8c973d7100d51628234dc1a11b8942b0f7b9
Soumik Rakshit <soumik@activesphere.com>
2019-05-27 15:23:50 +0530

    fix - inject in the tab of the sending script only

--- extension/background.js
+++ extension/background.js
@@ -1,25 +1,29 @@
+/*
+Only one vid-annot instance is allowed to run. Otherwise it's undefined what will happen.
+
+Two content scripts for better understanding.
+
+1. control.js - Per page. Loaded by background script. Checks if the vid-annot app is running in the
+page, if so it loads the worker.js script (via background script). It also listens to
+AppConfig.LoadControlScript message from the app and does the above thing.
+
+2. worker.js - All frame capture related thing is handled by the worker.js script.
+
+background.js - The single background script simply loads control.js and loads worker.js when
+control.js tells it to.
+
+*/
+
 import ns from './browser_namespace';
 
-const State = {
-    // On click, should first load the controller script
-    shouldLoadControllerScript: false,
-
-    controlScriptPort: null,
-
-    // Which tab the content script was injected. If closed, we reset the background scripts state
-    tabId: null,
-    windowId: null,
-};
-
 function connectedToControlScriptPort(p) {
-    State.controlScriptPort = p;
-    State.controlScriptPort.onMessage.addListener(m => {
+    p.onMessage.addListener(m => {
         console.log('Received message', m);
 
         switch (m.command) {
             case 'SHOULD_LOAD_INJECT_SCRIPT': {
                 console.log('Loading inject script');
-                ns.tabs.executeScript(State.tabId, {
+                ns.tabs.executeScript(p.sender.tab.id, {
                     file: 'worker.bundled.js',
                     allFrames: true,
                 });
