9b38a14003d514a2695e3afdeedbaad5e55eea34
ciju <ciju.ch3rian@gmail.com>
2019-05-29 01:44:21 +0530

    refactor: rename worker to yt-inject

--- extension/webpack.config.js
+++ extension/webpack.config.js
@@ -3,7 +3,7 @@ const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: {
-        worker: './worker.js',
+        'yt-inject': './yt-inject.js',
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
