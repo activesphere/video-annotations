e638d530404dca596144b349b9bed553423d8edc
Soumik Rakshit <soumik@activesphere.com>
2019-06-17 17:49:13 +0530

    Configurable keycode map

--- src/keycodeMap.js
+++ src/keycodeMap.js
@@ -1,8 +1,13 @@
-export default {
-    'control+p': 'togglePause',
-    'control+t': 'putTimestamp',
-    'control+s': 'saveNote',
-    'control+shift+right': 'videoForward',
-    'control+shift+left': 'videoBackward',
-    'control+shift+i': 'captureFrame',
+const keycodeOfCommandName = {
+  toggle_pause: 'Ctrl-p',
+  mark_selection_as_timestamp: 'Ctrl-t',
+  put_timestamp: 'Ctrl-Shift-t',
+  seek_to_timestamp: 'Ctrl-g',
+  capture_frame: 'Ctrl-i',
 };
+
+const commandNameOfKeycode = Object.fromEntries(
+  Object.entries(keycodeOfCommandName).map(([commandName, keycode]) => [keycode, commandName])
+);
+
+export { keycodeOfCommandName, commandNameOfKeycode };
