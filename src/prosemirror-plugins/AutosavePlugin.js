2bd4ac12b1efc4c0215d7ff73670a9866f783d1a
Soumik Rakshit <soumik@activesphere.com>
2019-06-17 13:23:50 +0530

    Using lodash.throttle

--- src/prosemirror-plugins/AutosavePlugin.js
+++ src/prosemirror-plugins/AutosavePlugin.js
@@ -1,60 +1,56 @@
-import throttle from '../utils/throttle';
+import throttle from 'lodash/throttle';
 import { save as saveToLocalStorage } from '../LocalStorageHelper';
 import { save as saveToDropbox } from '../DropboxHelper';
 
 const ThrottleIntervalMS = 3000;
 
 class AutosavePlugin {
-    constructor(view, videoId) {
-        this.didSaveToDropbox = false;
-        this.lastNoteData = null;
+  constructor(view, videoId) {
+    this.didSaveToDropbox = false;
+    this.lastNoteData = null;
 
-        this.throttledSave = throttle(ThrottleIntervalMS, noteData => {
-            saveToLocalStorage(noteData);
+    this.throttledSave = throttle(noteData => {
+      saveToLocalStorage(noteData);
 
-            this.promise = saveToDropbox(noteData);
+      this.promise = saveToDropbox(noteData);
 
-            this.promise
-                .then(() => {
-                    this.didSaveToDropbox = true;
-                })
-                .catch(() => {
-                    console.log('Error while saving to dropbox'); // eslint-disable-line no-console
-                });
+      this.promise
+        .then(() => {
+          this.didSaveToDropbox = true;
+        })
+        .catch(() => {
+          console.log('Error while saving to dropbox'); // eslint-disable-line no-console
         });
+    }, ThrottleIntervalMS);
 
-        window.onbeforeunload = this.beforeUnload;
+    window.onbeforeunload = this.beforeUnload;
 
-        this.view = view;
-        this.videoId = videoId;
-        this.update(view, null);
-    }
+    this.view = view;
+    this.videoId = videoId;
+    this.update(view, null);
+  }
 
-    beforeUnload = () => {
-        this.throttledSave.cancelPendingCall();
+  beforeUnload = () => {
+    this.throttledSave.flush();
+  };
 
-        if (this.lastNoteData) {
-            this.throttledSave.callOriginal(this.lastNoteData);
-        }
+  update() {
+    this.docJSON = this.view.state.doc.toJSON();
+
+    this.lastNoteData = {
+      videoId: this.videoId,
+      docJSON: this.docJSON,
+      timeOfSave: Date.now() / 1000.0,
     };
 
-    update() {
-        this.docJSON = this.view.state.doc.toJSON();
+    this.didSaveToDropbox = false;
 
-        this.lastNoteData = {
-            videoId: this.videoId,
-            docJSON: this.docJSON,
-            timeOfSave: Date.now() / 1000.0,
-        };
+    this.throttledSave(this.lastNoteData);
+  }
 
-        this.didSaveToDropbox = false;
-
-        this.throttledSave.update(this.lastNoteData);
-    }
-
-    destroy() {
-        window.removeEventListener('beforeunload', this.beforeUnload);
-    }
+  destroy() {
+    window.removeEventListener('beforeunload', this.beforeUnload);
+  }
 }
 
 export default AutosavePlugin;
