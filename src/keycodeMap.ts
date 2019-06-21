01bc9e8ac4c376e027e7f8aaef7db579e22eb23d
Soumik Rakshit <soumik@activesphere.com>
2019-06-21 17:35:21 +0530

    fromPairs - typescript version

--- src/keycodeMap.ts
+++ src/keycodeMap.ts
@@ -1,15 +1,17 @@
-const keycodeOfCommandName = {
-  toggle_pause: 'Ctrl-p',
-  mark_selection_as_timestamp: 'Ctrl-t',
-  turn_text_to_timestamp: 'Ctrl-y',
-  put_timestamp: 'Ctrl-Shift-t',
-  seek_to_timestamp: 'Ctrl-g',
-  capture_frame: 'Ctrl-i',
-  debug_print: 'Ctrl-d',
+import fromPairs from './utils/fromPairs';
+
+const keycodeOfCommandName: { [s: string]: string } = {
+	toggle_pause: 'Ctrl-p',
+	mark_selection_as_timestamp: 'Ctrl-t',
+	turn_text_to_timestamp: 'Ctrl-y',
+	put_timestamp: 'Ctrl-Shift-t',
+	seek_to_timestamp: 'Ctrl-g',
+	capture_frame: 'Ctrl-i',
+	debug_print: 'Ctrl-d',
 };
 
-const commandNameOfKeycode = Object.fromEntries(
-  Object.entries(keycodeOfCommandName).map(([commandName, keycode]) => [keycode, commandName])
+const commandNameOfKeycode = fromPairs(
+	Object.entries(keycodeOfCommandName).map(([commandName, keycode]) => [keycode, commandName])
 );
 
 export { keycodeOfCommandName, commandNameOfKeycode };
