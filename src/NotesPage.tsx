b871fc6ac1e3c0f67fbc535eecfd50d4a4bbec9c
ciju <ciju.ch3rian@gmail.com>
2019-06-21 17:29:31 +0530

    fix: import modules individually

--- src/NotesPage.tsx
+++ src/NotesPage.tsx
@@ -1,13 +1,12 @@
 import React, { useState, useEffect } from 'react';
-import {
-  Button,
-  Card,
-  CardActions,
-  CardContent,
-  CardMedia,
-  Typography,
-  Grid,
-} from '@material-ui/core';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+
 import CardActionArea from '@material-ui/core/CardActionArea';
 import classNames from 'classnames';
 import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
