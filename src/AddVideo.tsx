1c2c805e335c9f94388e2804af93a35f2c874d9e
ciju <ciju.ch3rian@gmail.com>
2019-06-26 13:19:40 +0530

    feat: move add video into its own component

---
+++ src/AddVideo.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import InputBase from '@material-ui/core/InputBase';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    search: {
+      position: 'relative',
+      borderRadius: theme.shape.borderRadius,
+      backgroundColor: 'hsl(0, 0%, 93%)',
+      '&:hover': {
+        backgroundColor: 'hsl(0, 0%, 90%)',
+      },
+      marginLeft: theme.spacing(1),
+      marginRight: theme.spacing(1),
+      transition: theme.transitions.create('all'),
+    },
+    searchFullWidth: {
+      flexGrow: 1,
+    },
+    searchIcon: {
+      width: theme.spacing(7),
+      height: '100%',
+      position: 'absolute',
+      pointerEvents: 'none',
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    inputRoot: {
+      color: 'inherit',
+      width: '100%',
+    },
+    inputInput: {
+      padding: theme.spacing(1, 1, 1, 7),
+    },
+  })
+);
+
+const AddVideo = () => {
+  const classes = useStyles();
+  const [searchFocused, setSearchFocus] = useState(false);
+  const searchFocus = () => setSearchFocus(true);
+  const searchBlur = () => setSearchFocus(false);
+
+  return (
+    <div className={classnames(classes.search, searchFocused && classes.searchFullWidth)}>
+      <div className={classes.searchIcon}>
+        <AddCircleOutlineIcon />
+      </div>
+      <InputBase
+        onFocus={searchFocus}
+        onBlur={searchBlur}
+        placeholder="https://youtube.com/watch?v=..."
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ 'aria-label': 'Search' }}
+      />
+    </div>
+  );
+};
+
+export default AddVideo;
--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,14 +1,12 @@
 import React, { useState } from 'react';
-import classnames from 'classnames';
 import { withRouter } from 'react-router-dom';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import DropboxSyncButton from './DropboxSyncButton';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
-import InputBase from '@material-ui/core/InputBase';
 import Button from '@material-ui/core/Button';
+import AddVideo from './AddVideo';
 
 import { Link } from 'react-router-dom';
 import { Location } from 'history';
@@ -21,36 +19,6 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     grow: {
       flexGrow: 1,
-    },
-    search: {
-      position: 'relative',
-      borderRadius: theme.shape.borderRadius,
-      backgroundColor: 'hsl(0, 0%, 93%)',
-      '&:hover': {
-        backgroundColor: 'hsl(0, 0%, 90%)',
-      },
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      transition: theme.transitions.create('all'),
-    },
-    searchFullWidth: {
-      flexGrow: 1,
-    },
-    searchIcon: {
-      width: theme.spacing(7),
-      height: '100%',
-      position: 'absolute',
-      pointerEvents: 'none',
-      display: 'flex',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    inputRoot: {
-      color: 'inherit',
-      width: '100%',
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 7),
     },
     tab: {
       minWidth: '6rem',
@@ -77,9 +45,6 @@ const useStyles = makeStyles((theme: Theme) =>
 
 const PrimarySearchAppBar = ({ location }: { location: Location }) => {
   const classes = useStyles();
-  const [searchFocused, setSearchFocus] = useState(false);
-  const searchFocus = () => setSearchFocus(true);
-  const searchBlur = () => setSearchFocus(false);
 
   const isHome = location.pathname === '/';
 
@@ -92,21 +57,7 @@ const PrimarySearchAppBar = ({ location }: { location: Location }) => {
             List
           </Button>
         )}
-        <div className={classnames(classes.search, searchFocused && classes.searchFullWidth)}>
-          <div className={classes.searchIcon}>
-            <AddCircleOutlineIcon />
-          </div>
-          <InputBase
-            onFocus={searchFocus}
-            onBlur={searchBlur}
-            placeholder="https://youtube.com/watch?v=..."
-            classes={{
-              root: classes.inputRoot,
-              input: classes.inputInput,
-            }}
-            inputProps={{ 'aria-label': 'Search' }}
-          />
-        </div>
+        <AddVideo />
         <div className={classes.grow} />
         <DropboxSyncButton />
       </Toolbar>
