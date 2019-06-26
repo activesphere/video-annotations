a52751737db531b03a8794c6957bc2c1dcfdc643
ciju <ciju.ch3rian@gmail.com>
2019-06-26 12:37:46 +0530

    feat: add css baseline

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import CssBaseline from '@material-ui/core/CssBaseline';
+
 import EditorPage from './EditorPage';
 import NotesPage from './NotesPage';
 import AppBar from './AppBar';
@@ -9,20 +11,23 @@ const Main = ({ ytAPI }: { ytAPI: any }) => {
   if (!ytAPI) return null;
 
   return (
-    <BrowserRouter>
-      <AppBar />
-      <Switch>
-        <Route
-          path="/v/:videoId"
-          render={({
-            match: {
-              params: { videoId },
-            },
-          }) => <EditorPage key={videoId} ytAPI={ytAPI} startingVideoId={videoId} />}
-        />
-        <Route path="/" component={NotesPage} />
-      </Switch>
-    </BrowserRouter>
+    <>
+      <CssBaseline />
+      <BrowserRouter>
+        <AppBar />
+        <Switch>
+          <Route
+            path="/v/:videoId"
+            render={({
+              match: {
+                params: { videoId },
+              },
+            }) => <EditorPage key={videoId} ytAPI={ytAPI} startingVideoId={videoId} />}
+          />
+          <Route path="/" component={NotesPage} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 };
 
