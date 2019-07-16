87168a9a081f1d739d5ef876b89da8418f91502f
ciju <ciju.ch3rian@gmail.com>
2019-07-16 15:00:52 +0530

    chore: update eslint and fix lint errors

--- src/utils/isYouTubeURL.ts
+++ src/utils/isYouTubeURL.ts
@@ -3,10 +3,10 @@ import isURL from 'is-url';
 const domains = ['www.youtube.com', 'youtube.com', 'youtu.be'];
 
 export default (str: string) => {
-    if (!isURL(str)) return false;
+  if (!isURL(str)) return false;
 
-    const { host } = new URL(str);
+  const { host } = new URL(str);
 
-    if (!domains.includes(host)) return false;
-    return true;
-}
+  if (!domains.includes(host)) return false;
+  return true;
+};
--- src/youtube/parseId.ts
+++ src/youtube/parseId.ts
@@ -1,15 +1,15 @@
 import isYouTubeId from './isId';
 
 export default (str: string) => {
-    if (!str) return null;
+  if (!str) return null;
 
-    const match = str.match(/[a-zA-Z0-9_-]{11}$/);
+  const match = str.match(/[a-zA-Z0-9_-]{11}$/);
 
-    if (!match || !match.length) return null;
+  if (!match || !match.length) return null;
 
-    const [id] = match;
+  const [id] = match;
 
-    if (!isYouTubeId(id)) return null;
+  if (!isYouTubeId(id)) return null;
 
-    return id;
-}
+  return id;
+};
